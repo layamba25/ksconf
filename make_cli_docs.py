fea45ff5acd4b49724d7ba32c8bd0301b5ad67ad
Lowell Alleman <lowell@kintyre.co>
2018-06-04 12:48:58 -0400

    Minor README cleanups
    - Add intro blurb
    - Show install as a 'pip' command (based on registration with PyPI)

--- make_cli_docs.py
+++ make_cli_docs.py
@@ -48,23 +48,30 @@ def write_doc_for(stream, cmd, level=2, cmd_name=None, *subcmds):
 
 readme = open("README.md.tmp", "w")
 readme.write("""\
-# Kintyre Splunk Configuration tool
+# Kintyre's Splunk CONFiguration tool
 
 [![Travis](https://img.shields.io/travis/Kintyre/ksconf.svg)](https://travis-ci.org/Kintyre/ksconf/builds)
 [![codecov](https://codecov.io/gh/Kintyre/ksconf/branch/master/graph/badge.svg)](https://codecov.io/gh/Kintyre/ksconf)
 [![Coverage Status](https://coveralls.io/repos/github/Kintyre/ksconf/badge.svg?branch=master)](https://coveralls.io/github/Kintyre/ksconf?branch=master)
 [![Windows Build status](https://ci.appveyor.com/api/projects/status/rlbgstkpf17y8nxh?svg=true)](https://ci.appveyor.com/project/lowell80/ksconf)
 
+This utility handles a number of common Splunk app maintenance tasks in an installable python
+package.  Specifically, this tools deals with many of the nuances with storing Splunk apps in a
+version control system like git and pointing live Splunk apps to a working tree, merging changes
+from the live system's (local) folder to the version controlled (default) folder, and dealing with
+more than one layer of "default" (which splunk can't handle natively).
+
 
 Install with
 
-    git clone https://github.com/Kintyre/ksconf.git
-    cd ksconf
-    pip install .
+    pip install KintyreSplunkConfTool
 
 Confirm installation with the following command:
 
     ksconf --help
+
+
+# Command line reference
 
 The following documents the CLI options
 
